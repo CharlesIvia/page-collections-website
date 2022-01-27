69362f4fd9426fc32374cdda8cd8ed7bdf97e07a
Charles Ivia <iviacharles@gmail.com>
2022-01-27 17:03:52 +0300

    updated data

--- src/data/BooksData.js
+++ src/data/BooksData.js
@@ -3,44 +3,59 @@
 import PromisedLandImg from "../assets/books/promised land.jpg";
 import LawsOfPowerImg from "../assets/books/laws of power.jpg";
 import IkiagaiImg from "../assets/books/ikiagai.jpg";
+import DrunkImg from "../assets/books/drunk.jpeg";
+import UnbowedImg from "../assets/books/Unbowed.jpg";
+import MorningClubImg from "../assets/books/morningclub.jpeg";
+import WillImg from "../assets/books/will.jpg";
+import AtomicImg from "../assets/books/atomic habits.jpg";
+import RainbowFishImg from "../assets/books/rainbow fish.jfif";
+import FamiliarMathImg from "../assets/books/familiarmath.jpg";
+import SapiensImg from "../assets/books/sapiens.jpg";
+import YellowSunImg from "../assets/books/yellow sun.jpg";
+import ShouldIImg from "../assets/books/should-i.jpeg";
 
 export const Books = [
   {
     name: "The Promised Land",
     author: "Barack Obama",
+    category: "biography",
     src: PromisedLandImg,
     popular: true,
     price: 1000,
     id: 0,
   },
   {
-    name: "The Promised Land",
-    author: "Barack Obama",
-    src: PromisedLandImg,
+    name: "Drunk",
+    author: "Jackson Biko",
+    category: "novels",
+    src: DrunkImg,
     popular: true,
     price: 1000,
     id: 1,
   },
   {
-    name: "The Promised Land",
-    author: "Barack Obama",
-    src: PromisedLandImg,
+    name: "Unbowed",
+    author: "Wangari Maathai",
+    category: "biography",
+    src: UnbowedImg,
     popular: true,
     price: 1000,
     id: 2,
   },
   {
-    name: "The Promised Land",
-    author: "Barack Obama",
-    src: PromisedLandImg,
+    name: "The 5A.M Club",
+    author: "Robbin Sharma",
+    category: "personalgrowth",
+    src: MorningClubImg,
     popular: true,
     price: 1000,
     id: 3,
   },
   {
-    name: "The Promised Land",
-    author: "Barack Obama",
-    src: PromisedLandImg,
+    name: "Will",
+    author: "Will Smith",
+    category: "biography",
+    src: WillImg,
     popular: true,
     price: 1000,
     id: 4,
@@ -48,6 +63,7 @@ export const Books = [
   {
     name: "The 48 Laws of Power",
     author: "Robert Green",
+    category: "nonfiction",
     src: LawsOfPowerImg,
     popular: true,
     price: 1200,
@@ -56,9 +72,64 @@ export const Books = [
   {
     name: "Ikiagai",
     author: "Hector Gracia",
+    category: "personalgrowth",
     src: IkiagaiImg,
     popular: false,
     price: 800,
     id: 6,
+  },
+  {
+    name: "Atomic Habits",
+    author: "James Clear",
+    category: "personalgrowth",
+    src: AtomicImg,
+    popular: true,
+    price: 800,
+    id: 7,
+  },
+  {
+    name: "Rainbow Fish",
+    author: "Marcus PFister",
+    category: "children",
+    src: RainbowFishImg,
+    popular: false,
+    price: 800,
+    id: 8,
+  },
+  {
+    name: "K.C.S.E Made Familiar Math",
+    author: "Caroline Njenga",
+    category: "textbook",
+    src: FamiliarMathImg,
+    popular: false,
+    price: 800,
+    id: 9,
+  },
+  {
+    name: "Sapiens",
+    author: "Noah Yuval",
+    category: "nonfiction",
+    src: SapiensImg,
+    popular: true,
+    price: 800,
+    id: 10,
+  },
+  {
+    name: "Half A Yellow Sun",
+    author: "Chimamanda Ngozi",
+    category: "novels",
+    src: YellowSunImg,
+    popular: false,
+    price: 800,
+    id: 11,
+  },
+  {
+    name: "Should I",
+    author: "Florence Bett",
+    category: "nonfiction",
+    src: ShouldIImg,
+    popular: false,
+    price: 800,
+    id: 12,
   },
 ];
--- src/data/categtoriesData.js
+++ src/data/categtoriesData.js
@@ -2,37 +2,44 @@
 import { HiTrendingUp } from "react-icons/hi";
 import { AiOutlineAccountBook } from "react-icons/ai";
 import { FaFantasyFlightGames } from "react-icons/fa";
-import { GiUnicorn } from "react-icons/gi";
+import { GiUnicorn, GiSwordwoman } from "react-icons/gi";
+import { IoSchoolOutline } from "react-icons/io5";
 
 export const categoriesData = [
   {
     name: "Personal Growth",
     src: HiTrendingUp,
+    link: "/personal-growth",
     id: 0,
   },
   {
     name: "Non Fiction",
     src: AiOutlineAccountBook,
+    link: "/non-fiction",
     id: 1,
   },
   {
     name: "Novels",
     src: FaFantasyFlightGames,
+    link: "/novels",
     id: 2,
   },
   {
     name: "Chldren Books",
     src: GiUnicorn,
+    link: "/children-books",
     id: 3,
   },
   {
     name: "Biographies",
-    src: FaFantasyFlightGames,
+    src: GiSwordwoman,
+    link: "/biographies",
     id: 4,
   },
   {
     name: "Textbooks",
-    src: FaFantasyFlightGames,
+    src: IoSchoolOutline,
+    link: "/textbooks",
     id: 5,
   },
 ];
