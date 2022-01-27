b74b6fb802330acf78f100fa1c5ad022a086334c
Charles Ivia <iviacharles@gmail.com>
2022-01-27 17:04:19 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import "./css/Normalize.css";
 import "./css/App.css";
 import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Cart } from "./components/Cart";
+import { Register } from "./components/Register";
+import { PersonalGrowth } from "./components/PersonalGrowth";
+import { ChildrenBooks } from "./components/ChildrenBooks";
+import { NonFiction } from "./components/NonFiction";
+import { Novels } from "./components/Novels";
+import { Biographies } from "./components/Biographies";
+import { Textbooks } from "./components/Textbooks";
 
+//Handling routing
 function App() {
   return (
     <div className="App">
@@ -12,6 +22,33 @@ function App() {
           <Switch>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/shopping-cart">
+              <Cart />
+            </Route>
+            <Route exact path="/register">
+              <Register />
+            </Route>
+            <Route exact path="/personal-growth">
+              <PersonalGrowth />
+            </Route>
+            <Route exact path="/non-fiction">
+              <NonFiction />
+            </Route>
+            <Route exact path="/novels">
+              <Novels />
+            </Route>
+            <Route exact path="/children-books">
+              <ChildrenBooks />
+            </Route>
+            <Route exact path="/biographies">
+              <Biographies />
+            </Route>
+            <Route exact path="/textbooks">
+              <Textbooks />
             </Route>
           </Switch>
         </div>
