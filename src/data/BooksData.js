001ee1a6cb64a54fa30382b2fed1318e7f65a482
Charles Ivia <iviacharles@gmail.com>
2022-02-01 12:55:16 +0300

    updated books data

--- src/data/BooksData.js
+++ src/data/BooksData.js
@@ -13,6 +13,7 @@ import FamiliarMathImg from "../assets/books/familiarmath.jpg";
 import SapiensImg from "../assets/books/sapiens.jpg";
 import YellowSunImg from "../assets/books/yellow sun.jpg";
 import ShouldIImg from "../assets/books/should-i.jpeg";
+import BecomingImg from "../assets/books/becoming.jpg";
 
 export const Books = [
   {
@@ -26,6 +27,17 @@ export const Books = [
     popular: true,
     price: 1000,
     id: 0,
+  },
+  {
+    name: "Becoming",
+    author: "Michelle Obama",
+    category: "biography",
+    description:
+      "In her memoir, a work of deep reflection and mesmerizing storytelling, Michelle Obama invites readers into her world, chronicling the experiences that have shaped her—from her childhood on the South Side of Chicago to her years as an executive balancing the demands of motherhood and work, to her time spent at the world’s most famous address. With unerring honesty and lively wit, she describes her triumphs and her disappointments, both public and private, telling her full story as she has lived it—in her own words and on her own terms. Warm, wise, and revelatory, Becoming is the deeply personal reckoning of a woman of soul and substance who has steadily defied expectations—and whose story inspires us to do the same.",
+    src: BecomingImg,
+    popular: true,
+    price: 1100,
+    id: 0.1,
   },
   {
     name: "Drunk",
